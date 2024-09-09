5be4f66c49da28a8c243926527dca4b7a45f7f5d
serijun1214 <serijun.1214+github@gmail.com>
2024-09-10 04:27:31 +0900

    複数画面の遷移を実装

---
+++ FileRelationship/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  FileRelationship
+//
+//  Created by 瀬利純樹 on 2024/09/10.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    @IBOutlet weak var label3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        label3.text = "3つめの画面"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
